f8364acfc5c1d8186c3150deefc7b2e2b8fb2e7d
ILIA <dubenskiiilia@gmail.com>
2022-11-05 14:11:35 +0100

    Initial Commit

---
+++ ApiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApiApp
+//
+//  Created by Илья Дубенский on 05.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
